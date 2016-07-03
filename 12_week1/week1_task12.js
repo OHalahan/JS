754a7728a02e2af1e7643c2bcc33693ac1e45de3
Oleksandr Halahan <Oleksandr.Halahan@portaone.com>
2016-07-03 11:02:16 +0300

    finished week1

---
+++ 12_week1/week1_task12.js
@@ -0,0 +1,7 @@
+function separate() {
+    var solid = document.getElementById("solid").value;
+    var groups = solid.toString().split(".");
+    groups[0] = groups[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
+    document.getElementById("out12").innerHTML = groups.join(".");
+}
